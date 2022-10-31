ad6e0a91853a4ac2a7d644b3e42e885cf3ee9191
Nitin Patel <patelnitin11111@gmail.com>
2022-10-30 23:46:44 -0400

    creation of main block and blank file functions

--- client.py
+++ client.py
@@ -1,10 +1,38 @@
 import socket
 
-HOST = socket.gethostbyname('localhost')
-PORT = 9090
+def main():
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST, PORT))
+    HOST = socket.gethostbyname('localhost')
+    PORT = 9090
 
-socket.send('Hello Geetha'.encode('utf-8'))
-print(socket.recv(1024).decode('utf-8'))
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect((HOST, PORT))
+
+    socket.send('Hello Geetha'.encode('utf-8'))
+    print(socket.recv(1024).decode('utf-8'))
+
+
+    while True:
+        print("Hello..")
+        client_message = input("Enter the command you want to perform: ")
+        client_message_0 = client_message.split()[0]
+        if client_message_0 == "ls":
+            pass
+        if client_message_0 == "create":
+            pass
+        if client_message_0 == "delete":
+            pass
+        if client_message_0 == "read":
+            pass
+        if client_message_0 == "write":
+            pass
+        if client_message_0 == "rename":
+            pass
+        if client_message_0 == "cd":
+            pass
+        if client_message_0 == "mkdir":
+            pass
+
+
+if __name__ == "__main__":
+    main()
