d8a326927213a780a85a457c4c47593de30f15f7
Aditi Joshi <aditijoshi@Aditis-MacBook-Air-2.local>
2022-11-04 21:08:11 -0400

    Adding ls and create functions

--- client.py
+++ client.py
@@ -1,5 +1,20 @@
 import socket
 
+def send_to_server(message):
+    # HOST = '10.200.137.77'
+    # HOST = socket.gethostbyname(socket.gethostname())
+    host = socket.gethostbyname('localhost')
+    port = 9090
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket.connect((host, port))
+    s_socket.send(message.encode('utf-8'))
+    message_recv_from_server = None
+    if (message.split('|')[0] in ["ls"]) or (
+            message.split(' ')[0] in ["create", "read", "cd", "delete", "mkdir", "write", "rename"]):
+        message_recv_from_server = s_socket.recv(1024)
+    s_socket.close()
+    return message_recv_from_server
+
 def main():
 
     HOST = socket.gethostbyname('localhost')
@@ -15,11 +30,12 @@ def main():
     while True:
         print("Hello..")
         client_message = input("Enter the command you want to perform: ")
+        message_recv_from_server = send_to_server(client_message)
         client_message_0 = client_message.split()[0]
         if client_message_0 == "ls":
             pass
         if client_message_0 == "create":
-            pass
+            print(message_recv_from_server)
         if client_message_0 == "delete":
             pass
         if client_message_0 == "read":
--- server.py
+++ server.py
@@ -1,24 +1,63 @@
 import socket
+import os
+
+def send_response_to_client(data, communication_socket):
+    communication_socket.send(data.encode('utf-8'))
+
+def listing_files_in_folder():
+    directory_path = "."
+
+    existing_files = [file for file in os.listdir(directory_path) if os.path.isfile(file) or os.path.isdir(file)]
+
+    file_list = ""
+    for file in existing_files:
+        file_list += file + "\n"
+    return file_list
+
+def creating_file(wanted_filename, communication_socket, client_address):
+    # content = write(filename, communication_socket, server, current_dir)
+
+    if wanted_filename not in (listing_files_in_folder()):
+        with open(wanted_filename, "w") as f:
+            print("Created the file in Server 1")
+            data = "Created the file in Server 1"
+            send_response_to_client(data, communication_socket)
+    else:
+        data = "File already exist"
+        send_response_to_client(data, communication_socket)
+    communication_socket.close()
+    print(f'Communication with {client_address} ended!')
+
 
 def main():
 
-    #HOST = '10.200.137.77'  # 'localhost'
-    HOST = socket.gethostbyname('localhost')
-    PORT = 9090
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(5)
+    host = socket.gethostbyname('localhost')
+    # host = '130.85.243.2'
+    port = 9090
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket.bind((host, port))
+    s_socket.listen(5)
+    main_dir = os.getcwd()
+    print('Server 1 is listening!......')
 
     while True:
-        communication_socket, address = server.accept()
-        print(f'Connected to {address}')
-        message = communication_socket.recv(1024).decode('utf-8')
-        print(f'Message from client is: {message}')
-        communication_socket.send('Got your message. Thank you!'.encode('utf-8'))
-        communication_socket.close()
-        print(f'Communication with {address} ended!')
+        communication_socket, client_address = s_socket.accept()
+        print(f'Connected to {client_address}')
+        # getting username
+        # getting password
+        # authorizing the client or users
+        # users exist? or else create new directory
 
+        print("Processing the message received from client...")
+        client_message = communication_socket.recv(1024).decode('utf-8')
+        print(f'Message from client is: {client_message}')
+
+        client_message_0 = client_message.split()[0]
+        if len(client_message.split()) > 1:
+            wanted_filename = client_message.split()[1]
+
+        if client_message_0 == "create":
+            creating_file(wanted_filename, communication_socket, client_address)
 
 if __name__ == "__main__":
     main()
